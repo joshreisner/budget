6c283ba83be986155e4d671acab4267510c000ed
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2026-08-17 08:32:31 -0700

    fix missing env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import netlify from "@netlify/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [netlify(), react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  // Assign the loaded .env variables directly to Node's process environment
+  process.env = { ...process.env, ...env };
+
+  return {
+    plugins: [netlify(), react(), tailwindcss()],
+  };
 });
