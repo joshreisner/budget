1d371c940042fdcf1a228c2261ed327b8dc4d699
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2026-08-17 08:27:20 -0700

    spinner visual tweak

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export const Spinner = () => (
   <div className="flex justify-center items-center min-h-dvh">
     <div
-      className="border-4 border-gray-500 border-t-transparent rounded-full w-12 h-12 animate-spin"
+      className="border-6 border-gray-300 dark:border-gray-700 border-t-gray-900 dark:border-t-gray-200 rounded-full w-14 h-14 animate-spin"
       role="status"
     >
       <span className="sr-only">Loading...</span>
